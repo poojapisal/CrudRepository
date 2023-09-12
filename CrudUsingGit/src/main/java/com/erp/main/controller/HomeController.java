d323cb3d0f3b72bfe0ec191a1e1fa58a9b9dea36
ajayrande <ajay16rande@gmail.com>
2023-09-12 16:20:20 +0530

    controller code update

--- CrudUsingGit/src/main/java/com/erp/main/controller/HomeController.java
+++ CrudUsingGit/src/main/java/com/erp/main/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.erp.main.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.erp.main.model.Student;
 
 @RestController
 public class HomeController {
@@ -11,5 +16,15 @@ public class HomeController {
 	public String getData()
 	{
 		return "Demo Git Project";
+	}
+	
+	@PostMapping("/savestudent")
+	public String saveData(@RequestBody Student s)
+	{
+		System.out.println(s.getName());
+		System.out.println(s.getRollno());
+		System.out.println(s.getAddress());
+		return "save Student";
+		
 	}
 }
