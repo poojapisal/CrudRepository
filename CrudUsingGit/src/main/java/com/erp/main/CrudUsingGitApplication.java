401b89f47ab172c3f849138dd650a32c5466d72d
poojapisal <shindepooja2496@gmail.com>
2023-09-12 15:36:05 +0530

    inital commit

---
+++ CrudUsingGit/src/main/java/com/erp/main/CrudUsingGitApplication.java
@@ -0,0 +1,13 @@
+package com.erp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudUsingGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudUsingGitApplication.class, args);
+	}
+
+}
